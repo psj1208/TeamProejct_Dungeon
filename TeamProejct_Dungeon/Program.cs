a2a8e128f63fe67b631daa61eaa905b2e583ebed
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-02-06 19:55:27 +0900

    [feat] 메소드 지정
    // 전투 시작
    static void Battle_Start(ICharacter player, ICharacter monster)
    // 공격
    static void Battle_Player(ICharacter player, ICharacter monster)
    // Enemy Pahse
    static void Battle_Enemy(ICharacter player, ICharacter monster)
    // 전투 결과
    static void Battle_End(ICharacter player, ICharacter monster)

--- TeamProejct_Dungeon/Program.cs
+++ TeamProejct_Dungeon/Program.cs
@@ -17,6 +17,30 @@
         static void Battle(ICharacter player,ICharacter monster)
         {
             //여기에 배틀.
+
+
+        }
+
+        static void Battle_Start(ICharacter player, ICharacter monster)
+        {
+            // 전투 시작
+
+
+        }
+        // 공격
+        static void Battle_Player(ICharacter player, ICharacter monster)
+        {
+
+        }
+        // Enemy Pahse 
+        static void Battle_Enemy(ICharacter player, ICharacter monster)
+        {
+
+        }
+        // 전투 결과
+        static void Battle_End(ICharacter player, ICharacter monster)
+        {
+
         }
     }
 }
