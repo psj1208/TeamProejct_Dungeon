5584c4e8e8672164731e657435c2d436816f18d4
alsoox <alsoox173@gmail.com>
2025-02-07 20:13:17 +0900

    [feat] 강순종_Skill 업데이트

--- TeamProejct_Dungeon/SkillManager.cs
+++ TeamProejct_Dungeon/SkillManager.cs
@@ -26,30 +26,36 @@ namespace TeamProejct_Dungeon
     {
         public WarriorSkill() : base("전사 스킬", "전사 스킬 설명 ") { }
 
-        public void Warriorskill1(Player player, Monster monster)
-        {
-            int damage = player.atk * 2;
-            monster.TakeDamage(damage);
-            Console.WriteLine($"{player.Name}이(가) {monster.Name}에게 '강력한 일격' 사용! {damage} 피해를 입혔다!");
-            Console.ReadLine();
-        }
-
-
         public override void Use(Player player, List<Monster> monsters)
         {
-            Console.WriteLine("\n1. 강력한 일격 (단일 대상 공격)");
+            Console.WriteLine("사용할 스킬을 선택하세요.");
+            Console.WriteLine("1. 워리어 스킬");
+            Console.WriteLine("2. 워리어 스킬");
 
             int skillChoice = Text.GetInput(null, 1);
 
+            Console.WriteLine("\n공격할 몬스터를 선택하세요.");
+            List<Monster> selcetMonsters = Text.GetInputMulti(2, monsters);
+
             if (skillChoice == 1)
             {
-                Console.WriteLine("\n공격할 몬스터를 선택하세요.");
-                int monsterChoice = Text.GetInput(null, monsters.Count);
-                Warriorskill1(player, monsters[monsterChoice-1]);
+                Warriorskill1(player, selcetMonsters);
             }
         }
 
+        public void Warriorskill1(Player player, List<Monster> monsters)
+        {
+            foreach (Monster monster in monsters)
+            {
+                int damage = player.atk * 2;
+                Console.WriteLine($"전사 스킬1 사용! {monster.Name}에게 {damage}의 피해를 입혔습니다.");
+                monster.TakeDamage(damage);
+            }
+        }
     }
+
+
+}
 
     //public class AssassinSkill : Skill
     //{
@@ -66,4 +72,4 @@ namespace TeamProejct_Dungeon
     //        }
     //    }
     //}
-}
+
