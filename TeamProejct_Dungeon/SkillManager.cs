912c7451c3a077bc2cfa385c18ca228afde91d92
alsoox <alsoox173@gmail.com>
2025-02-07 20:52:56 +0900

    [feat] 겅순종_Skill 추가

--- TeamProejct_Dungeon/SkillManager.cs
+++ TeamProejct_Dungeon/SkillManager.cs
@@ -29,22 +29,21 @@ namespace TeamProejct_Dungeon
         public override void Use(Player player, List<Monster> monsters)
         {
             Console.WriteLine("사용할 스킬을 선택하세요.\n");
-            Console.WriteLine("1. 확정 2배");
-            Console.WriteLine("2. 1~3배 랜덤");
+            Console.WriteLine("1. 파워 어택!! : 2배의 데미지로 공격 합니다.");
+            Console.WriteLine("2. 운수 좋은 날~ : 1~3배의 데미지로 공격합니다.");
 
             Console.WriteLine("\n공격할 몬스터를 선택하세요.");
 
             int skillChoice = Text.GetInput(null, 1);
             
-            List<Monster> selcetMonsters = Text.GetInputMulti(1, monsters);
-
             if (skillChoice == 1)
             {
-                
+                List<Monster> selcetMonsters = Text.GetInputMulti(1, monsters);
                 Warriorskill1(player, selcetMonsters);
             }
             else if (skillChoice == 2)
             {
+                
                 Console.WriteLine("\n공격할 몬스터를 선택하세요.");
             }
         }
@@ -53,15 +52,24 @@ namespace TeamProejct_Dungeon
         {
             foreach (Monster monster in monsters)
             {
+                
                 int damage = player.atk * 2;
-                Console.WriteLine($"전사 스킬1 사용! {monster.Name}에게 {damage}의 피해를 입혔습니다.");
+                Console.WriteLine($"파워 어택!! {monster.Name}에게 {damage}의 피해를 입혔습니다.");
                 monster.TakeDamage(damage);
             }
         }
 
         public void Warriorskill2(Player player, List<Monster> monsters)
         {
-            
+            foreach (Monster monster in monsters)
+            {
+                Random random = new Random();
+                int randomDamage = random.Next(1, 4);
+
+                int damage = player.atk * randomDamage;
+                Console.WriteLine($"운수 좋은 날~ {monster.Name}에게 {damage}의 피해를 입혔습니다.");
+                monster.TakeDamage(damage);
+            }
         }
     }
 
