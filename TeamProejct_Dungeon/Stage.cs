2be2a1e959f161ef053bcdac20dc7f5a3f8724b6
lhgenol <lhgenol@gmail.com>
2025-02-10 11:01:41 +0900

    [feat] 이유림_Monster 몬스터 수 추가

--- TeamProejct_Dungeon/Stage.cs
+++ TeamProejct_Dungeon/Stage.cs
@@ -14,13 +14,15 @@ namespace TeamProejct_Dungeon
         }
     }
 
-    public static class StageDb
+    // 스테이지 데이터베이스
+    public static class StageDB
     {
         //for문 돌려서 0~4,5~9,10~14 이렇게 순서대로 넣으셔도 되고. 임의로 넣으셔고 되고. 수정 가하셔도됩니다.
         //예시
-        public static List<Stage> St = new List<Stage>
+        public static List<Stage> StageList = new List<Stage>
         {
             new Stage(MonsterDB.GetMonsters(),StageClear.ClearOne)
+
         };
     }
 
